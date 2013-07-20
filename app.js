f9c00a2b7a77eb9646b84de3e70ed62e043ae91d
Javier Li <jli@alivebox.com>
2013-07-20 15:53:09 -0600

    implements carousel view to app

--- app.js
+++ app.js
@@ -23,7 +23,9 @@ Ext.application({
     name: 'UPSSATravelDemo',
 
     requires: [
-        'Ext.MessageBox'
+        'Ext.MessageBox',
+        'Ext.carousel.Carousel',
+        'Ext.Img'
     ],
 
     views: [
@@ -37,34 +39,77 @@ Ext.application({
         '144': 'resources/icons/Icon~ipad@2x.png'
     },
 
-    isIconPrecomposed: true,
-
     startupImage: {
-        '320x460': 'resources/startup/320x460.jpg',
-        '640x920': 'resources/startup/640x920.png',
-        '768x1004': 'resources/startup/768x1004.png',
-        '748x1024': 'resources/startup/748x1024.png',
-        '1536x2008': 'resources/startup/1536x2008.png',
-        '1496x2048': 'resources/startup/1496x2048.png'
+        '320x460': 'resources/startup/Default.jpg', // Non-retina iPhone, iPod touch, and all Android devices
+        '640x920': 'resources/startup/640x920.png', // Retina iPhone and iPod touch
+        '640x1096': 'resources/startup/640x1096.png', // iPhone 5 and iPod touch (fifth generation)
+        '768x1004': 'resources/startup/768x1004.png', //  Non-retina iPad (first and second generation) in portrait orientation
+        '748x1024': 'resources/startup/748x1024.png', //  Non-retina iPad (first and second generation) in landscape orientation
+        '1536x2008': 'resources/startup/1536x2008.png', // : Retina iPad (third generation) in portrait orientation
+        '1496x2048': 'resources/startup/1496x2048.png' // : Retina iPad (third generation) in landscape orientation
     },
 
+    isIconPrecomposed: false,
+
+    glossOnIcon: false,
+    phoneStartupScreen: 'resources/startup/640x1096.png',
+    tabletStartupScreen: 'resources/startup/640x1096.png',
+    /**
+     * The launch method is called when the browser is ready and the application is ready to
+     * launch.
+     */
     launch: function() {
-        // Destroy the #appLoadingIndicator element
-        Ext.fly('appLoadingIndicator').destroy();
+        //first we define each of the categories we have for each one of the horixontal carousels
+        //these images can be found inside resources/photos/{category_name}/*
+        var categories = ['Food', 'Animals', 'Cars', 'Architecture'],
+            itemsCountPerCategory = 10,
+            horizontalCarousels = [],
+            items, i, j, ln, category;
 
-        // Initialize the main view
-        Ext.Viewport.add(Ext.create('UPSSATravelDemo.view.Main'));
-    },
+        //now we loop through each of the categories
+        for (i = 0,ln = categories.length; i < ln; i++) {
+            items = [];
+            category = categories[i];
 
-    onUpdated: function() {
-        Ext.Msg.confirm(
-            "Application Update",
-            "This application has just successfully been updated to the latest version. Reload now?",
-            function(buttonId) {
-                if (buttonId === 'yes') {
-                    window.location.reload();
-                }
+            for (j = 1; j <= itemsCountPerCategory; j++) {
+                //and push each of the image as an item into the items array
+                //you can see we are using the img xtype which is an image component,
+                //and we just give is a custom cls to style it, and the src
+                //of the image
+                items.push({
+                    xtype: 'image',
+                    cls: 'my-carousel-item-img',
+                    src: 'resources/photos/' + category + '/' + j + '.jpg'
+                });
             }
-        );
+
+            //now we add the new horizontal carousel for this category
+            horizontalCarousels.push({
+                xtype: 'carousel',
+
+                //the direction is horizontal
+                direction: 'horizontal',
+
+                //we turn on direction lock so you cannot scroll diagonally
+                directionLock: true,
+
+                //and give it the items array
+                items: items
+            });
+        }
+
+        //and finally we create the vertical carousel which contains each of the horizontal
+        //category carousels above
+        Ext.Viewport.add({
+            xtype: 'carousel',
+
+            bufferSize: 2,
+
+            //this time direction vertical
+            direction: 'vertical',
+
+            //and the horizontalCarousels array
+            items: horizontalCarousels
+        });
     }
 });
